94e7e6fc5004a7c86cdc3cbc3a4b1013ea8d2f82
Luyang Liu <https://github.com/luyangliuable>
2021-11-23 23:23:10 +1100

    Succesfully added code that allows files to be found using wildcard*

--- comment-extractor/app.py
+++ comment-extractor/app.py
@@ -7,26 +7,35 @@ T = TypeVar("T")
 def extract_comment_from_line(file: str) -> str:
     return comment_parser.extract_comments(file, mime='text/x-python')
 
-
-
-def searchFile(fileName: str, path: str):
-    print(path)
+def searchFile(fileName: str, path: str) -> List[T]:
+    print("Searching in path: " + path + " for " + fileName)
+    res = []
     for root, dirs, files in os.walk(path):
-        print(files)
-        print(root)
-        print('Looking in:',root)
-        for Files in files:
-            try:
-                found = Files.find(fileName)
-                # print(found)
-                if found != -1:
-                    print()
-                    print(fileName, 'Found\n')
-                    break
-            except:
-                exit()
+        if fileName[0] == '*':
+            for Files in files:
+                sameFormat = checkFileSameFormat(fileName, Files)
+                if sameFormat:
+                    # res.append(Files)
+                    res.append("./" + (dirs[0] + "/" if len(dirs) > 0 else "") + Files)
+        else:
+            found = Files.find(fileName)
+            if found != -1:
+                # res.append(fileName)
+                break
+    return res
+
+def checkFileSameFormat(fileOne: str, fileTwo:str) -> bool:
+    counter = 1
+    while fileOne[-counter] != "." and fileTwo[-counter] != "." and counter <= min(len(fileOne), len(fileTwo)):
+        if fileOne[-counter] != fileTwo[-counter]:
+            return False
+        counter += 1
+
+    return True
+
 
 f = open("script.py", "r")
-searchFile('script.py', './test_folder')
+print(checkFileSameFormat("a.py", "b.py"))
+print(searchFile('*.py', './'))
 # print(f.read())
 # print(extract_comment_from_line('./test-folder/script.py'))
