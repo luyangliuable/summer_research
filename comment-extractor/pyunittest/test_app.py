d16093e35718d870cb0e98be5b2bba07820b3587
Luyang Liu <lli240@student.monash.edu>
2021-11-30 15:38:53 +1100

    Updated unittests

--- comment-extractor/pyunittest/test_app.py
+++ comment-extractor/pyunittest/test_app.py
@@ -115,6 +115,39 @@ class test_app(unittest.TestCase):
         test_case = app.save_in_dict('line', 'location', 'language')
         self.assertTrue(test_case, {"line": 'line', "location": 'location', 'language': 'language'})
 
+    # def test_wrong_files_c(self):
+    #     location = "./test-folder/test.c"
+    #     test_case = app.get_every_line_from_file(location)
+    #     self.assertEqual(test_case[-1], {'line': "#include <linux/parman.h>", 'location': './test-folder/test.c', 'language': 'c'})
+
+    def test_extract_wrong_comments_c(self):
+        location = "./test-folder/test.c"
+        test_case = app.get_every_line_from_file(location)
+        test_case = app.extract_comment_from_line_list(test_case, app.c_comment)
+        self.assertNotEqual(test_case[-1]['line'], "wrong comment")
+
+    def test_write_wrong_comments_c(self):
+        comment = [{"line": "/* * lib/parman.c - Manager for linear priority array areas */", "location": "random", "language": 'c'}, {"line": "/* test 1 */", "location": "random", "language": 'c'}]
+
+        location = app.create_comment_file('./', app.c_comment)
+        app.write_comment_file(comment, location)
+
+    def test_full_write_file(self):
+        file = "./test-folder/parman.c"
+        comment_dir = './dump.csv'
+        lines_in_file = app.get_every_line_from_file(file)
+        comments_in_file = app.extract_comment_from_line_list(lines_in_file, app.c_comment)
+
+        # Strip comment of symbols ####################################################
+        comments = [app.save_in_dict(app.strip_comment_of_symbols(comment['line'], app.c_comment), comment['location'], app.c_comment) for comment in comments_in_file]
+        # comments = [{'line': strip_comment_of_symbols(comment['line'], language), 'location': comment['location']} for comment in comments_in_file]
+
+        # Strip comment of starting whitespace ########################################
+        # comments = [remove_starting_whitespace(comments[i]['line']) for i in range(len( comments ))]
+        comments = [app.save_in_dict(app.remove_starting_whitespace(comment['line']), comment['location'], app.c_comment['language']) for comment in comments]
+
+        app.write_comment_file(comments, comment_dir)
+
 def main():
     # Create a test suit
     suit = unittest.TestLoader().loadTestsFromTestCase(test_app)
